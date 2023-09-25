95835e42469e3c81f12c2a40a637923fea0d5627
Paolo Burgio <paolo.burgio@gmail.com>
2023-09-25 12:14:15 +0200

    Modified solution

--- Code/automata-1/automata-1-solution.cpp
+++ Code/automata-1/automata-1-solution.cpp
@@ -16,10 +16,10 @@ char next()
 
 /*
  * Computes next state. Returns the following
- *   >0 : the next state
- *   -1 : input sequence not legal
- *   -2 : we're in final state
- *   -3 : internal error
+ *   >=0 : the next state
+ *   -1  : input sequence not legal
+ *   -2  : we're in final state
+ *   -3  : internal error
  */
 int nextState(int currState, char input)
 {
